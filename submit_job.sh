980a57a63de73564f15cc1a48837dc09dfda3fbd
Morad Elsaify <elsaifym@gmail.com>
2020-03-28 16:59:39 -0400

    Misc changes

--- submit_job.sh
+++ submit_job.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 #SBATCH -p scavenger
-#SBATCH -e Slurm/slurm_%A_%a.err
-#SBATCH -o Slurm/slurm_%A_%a.out 
+#SBATCH -e slurm/slurm_%A_%a.err
+#SBATCH -o slurm/slurm_%A_%a.out 
 #SBATCH --mem=10G
-#SBATCH --array=1-13292
+#SBATCH --array=1-13322
 
-cd '/hpc/group/fuqua/mie4/EDGAR Parsing'
+#  NOTE: MUST CHANGE THE ARRAY SIZE TO BE EQUAL TO THE NUMBER OF FILES IN THE DIRECTORY
+
+# change directory, create slurm and rout directories
+cd '/hpc/group/fuqua/mie4/edgar_parsing'
+mkdir 'slurm'
+mkdri 'rout'
+
+# load R
 module load R/3.6.0
-R CMD BATCH 'Code/3_process_one_cik.R'
+
+# run R command, store out file
+R CMD BATCH --no-restore --no-save code/3_process_one_cik.R rout/rout_$SLURM_ARRAY_TASK_ID.Rout
